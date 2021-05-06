e00e181162317ae0e8abbbabf2d992ccc13b5796
Bhavika Khare <bhavika.khare@gmail.com>
2021-05-06 00:20:50 -0500

    partial new page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,25 @@
 class AnswersController < ApplicationController
+
+    def new
+        @job_posting = JobPosting.find(params[:posting_id])
+        @job_application = JobApplication.find(params[:id])
+        @questions = @job_posting.questions
+        @answer = Answer.new
+        @answer.job_application = @job_application
+        @answers = @job_application.answers
+        render :new
+    end
+
+    def create
+        @job_posting = JobPosting.find(params[:id])
+        @question = @job_posting.questions.build(params.require(:question).permit(:question))
+        if @question.save
+            flash[:success] = "Question saved successfully"
+            redirect_to new_jp_question_url(@job_posting)
+        else
+            flash.now[:error] = "Question could not be saved"
+            redirect_to new_jp_question_url(@job_posting)
+        end
+    end
+
 end
